06394fff3ae16cef3f3114f9f1638b13f7e79a2f
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-28 13:06:27 +0530

    Added : Node.java and LinkedListDemo.java files added

---
+++ src/com/linkedList/LinkedListDemo.java
@@ -0,0 +1,51 @@
+package com.linkedList;
+
+public class LinkedListDemo<T> {
+	Node<T> head;
+
+	// add -first Node
+	public void addFirst(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		newNode = head.next;
+		head = newNode;
+	}
+
+	// add -Last Node
+	public void addLast(T data) {
+		Node<T> newNode = new Node<>(data);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		Node<T> currentNode = head;
+		while (currentNode.next != null) {
+			currentNode = currentNode.next;
+		}
+		currentNode.next = newNode;
+	}
+	//print List Method
+	public void printList() {
+		if (head == null) {
+			System.out.println(" List Is Empty .");
+			return;
+		}
+		Node<T> currentNode = head;
+		while (currentNode.next != null) {
+			System.out.println(currentNode + " ->");
+			currentNode = currentNode.next;
+		}
+		System.out.println(" null");
+	}
+
+	public static void main(String[] args) {
+
+		LinkedListDemo<Integer> linkedListDemo = new LinkedListDemo<Integer>();
+		
+		
+	}
+
+}
---
+++ src/com/linkedList/Node.java
@@ -0,0 +1,14 @@
+package com.linkedList;
+
+public class Node<T> {
+	T data;
+	Node<T> next;
+	public Node() {
+		super();
+	}
+	public Node(T data) {
+		super();
+		this.data = data;
+		this.next = null;
+	};
+}
